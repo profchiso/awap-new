f5d6a2e1a7beab6448de0e0b22692b54a481a3b6
Zonnaslyric <echezonnachukwu@gmail.com>
2021-02-15 01:36:15 -0800

    project initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import LandingPage from "./Pages/LandingPage";
+// import MobileNav from "./components/SideNav/MobileNav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const theme = createMuiTheme({
+  overrides: {
+    pallete: {
+      primary: "#0D065F",
+      // secondary: "#FF5757",
+    }
+  }
+})
+
+
+class App extends React.Component {
+  render() {
+    return (
+        <Router>
+        <ThemeProvider theme={theme}>
+          {/* <Provider store={store}> */}
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+            </Switch>
+          {/* </Provider> */}
+          </ThemeProvider>
+        </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/LandingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LandingPage() {
+    return (
+        <div className="border border-red-500">
+            Hello world
+        </div>
+    )
+}
