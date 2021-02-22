293d754345a9403481bc9583ae056923c6ced3ac
Zonnaslyric <echezonnachukwu@gmail.com>
2021-02-21 19:42:10 -0800

    update

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import HomeBody from "../components/HomeBody";
+import HomeBody from "../components/HomeBody/index";
 
 export default function LandingPage() {
   return (
