7316cbbd4695b5a234334b6f69618d01ba2c46cc
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-03-02 07:01:32 +0100

    setup redux, initial reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
-import LandingPage from "./Pages/LandingPage";
-import Login from "./Pages/login/Login"
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../src/store';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import LandingPage from './Pages/LandingPage';
+import Login from './Pages/login/Login';
 import 'react-multi-carousel/lib/styles.css';
-
 
 const theme = createMuiTheme({
   palette: {
@@ -12,22 +13,23 @@ const theme = createMuiTheme({
       main: '#0D065F',
     },
   },
-})
-
+});
 
 class App extends React.Component {
   render() {
     return (
+      <Provider store={store}>
         <Router>
-        <ThemeProvider theme={theme}>
-          {/* <Provider store={store}> */}
+          <ThemeProvider theme={theme}>
+            {/* <Provider store={store}> */}
             <Switch>
               <Route exact path="/" component={LandingPage} />
               <Route exact path="/login" component={Login} />
             </Switch>
-          {/* </Provider> */}
+            {/* </Provider> */}
           </ThemeProvider>
         </Router>
+      </Provider>
     );
   }
 }
