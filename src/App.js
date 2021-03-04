002b610b53d844ef00f99a8632bd191c02788827
Zonnaslyric <echezonnachukwu@gmail.com>
2021-03-04 11:21:45 -0800

    merged with login branch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from '../src/store';
-import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import LandingPage from './Pages/LandingPage';
-import Login from './Pages/login/Login';
-import 'react-multi-carousel/lib/styles.css';
-// <<<<<<< Login-screen
-// =======
-// import UntimedPQ from "./Pages/UntimedPQ";
-// import ChooseYear from "./Pages/ChooseYear";
-// import Login from "./Pages/Login"
-// import ProtectedRoute from "./Pages/ProtectedRoute";
-// >>>>>>> main
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../src/store";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import LandingPage from "./Pages/LandingPage";
+import "react-multi-carousel/lib/styles.css";
+import UntimedPQ from "./Pages/UntimedPQ";
+import ChooseYear from "./Pages/ChooseYear";
+import Login from "./Pages/Login";
+import ProtectedRoute from "./Pages/ProtectedRoute";
 
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#0D065F',
+      main: "#0D065F",
     },
   },
 });
@@ -31,13 +27,9 @@ class App extends React.Component {
             {/* <Provider store={store}> */}
             <Switch>
               <Route exact path="/" component={LandingPage} />
-// <<<<<<< Login-screen
-//               <Route exact path="/login" component={Login} />
-// =======
-//               <Route path="/login" component={Login} />
-//               <ProtectedRoute path="/untimed-pq" component={UntimedPQ} />
-//               <ProtectedRoute path="/choose-year" component={ChooseYear} />
-// >>>>>>> main
+              <Route exact path="/login" component={Login} />
+              <ProtectedRoute path="/untimed-pq" component={UntimedPQ} />
+              <ProtectedRoute path="/choose-year" component={ChooseYear} />
             </Switch>
             {/* </Provider> */}
           </ThemeProvider>
