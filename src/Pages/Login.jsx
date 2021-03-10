cfb522e7590bb5577b260ed7a879a76355ceadde
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-03-10 03:28:57 +0100

    re-add login implementation

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import LoginHeader from '../components/Auth/LoginHeader';
 import LoginBody from '../components/Auth/LoginBody';
-import { Redirect } from 'react-router-dom';
 
-export default function Login(props) {
+import { login, saveLoginUserDataToState } from '../redux/actions/login';
+
+function Login({ login }) {
   // const isAuthenticated = false;
 
   return (
     <div className="bg-f8 pb-8">
       {localStorage.token ? <Redirect to="/pq/biology-choose-year" /> : null}
       <LoginHeader />
-      <LoginBody />
+      <LoginBody loginRequest={login} />
     </div>
   );
 
@@ -23,3 +25,13 @@ export default function Login(props) {
   //   <Redirect to={props.component} />
   // );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+export default connect(mapStateToProps, {
+  login,
+  saveLoginUserDataToState,
+})(Login);
