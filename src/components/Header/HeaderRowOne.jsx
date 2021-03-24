e49d5ed76bae283e0484fd51f624f30831498e10
Mmojekwu Cyril <lovelyjhames@gmail.com>
2021-03-23 21:03:56 -0700

    update

--- src/components/Header/HeaderRowOne.jsx
+++ src/components/Header/HeaderRowOne.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {connect} from "react-redux"
+import { connect } from "react-redux";
 import Search from "../Search/Search";
 import { ReactComponent as AwesumEdgeLogo } from "../../assets/svgs/AwesumEdgeLogo.svg";
 import awesumBook from "../../assets/svgs/AwesumBook.svg";
@@ -7,12 +7,41 @@ import { Link } from "react-router-dom";
 import { ReactComponent as AwesumQuiz } from "../../assets/svgs/AwesumQuiz.svg";
 import { ReactComponent as Filter } from "../../assets/svgs/FilterIcon.svg";
 //import {loginReducer} from "../../redux/reducers/loginReducer"
-
+import { makeStyles } from "@material-ui/core/styles";
 import { CircleUserAvatar } from "../Avatar/Avatar";
 import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
+import { Button, Menu, MenuItem } from "@material-ui/core";
+import { FiLogOut } from "react-icons/fi";
 
- function HeaderRowOne({ showFilter,loginReducer }) {
-  const {user}= loginReducer
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  menuItem: {
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+}));
+
+function HeaderRowOne({ showFilter, loginReducer }) {
+  const classes = useStyles();
+
+  const { user } = loginReducer;
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  function handleClick(event) {
+    if (anchorEl !== event.currentTarget) {
+      setAnchorEl(event.currentTarget);
+    }
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+
   return (
     <div className="max-w-screen-2xl mx-auto px-6 lg:px-16">
       <div className="flex items-center my-6">
@@ -26,17 +55,57 @@ import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
         </div>
         <Search />
         {localStorage.token ? (
-            // {login ? (
+          // {login ? (
           <div className="flex items-center gap-5">
             <button className="flex items-center  font-body">
               <AwesumQuiz />
               <span className="pl-2 text-primary">Awesum Quiz</span>
             </button>
             <button className="flex items-center  font-body">
-              <CircleUserAvatar imgUrl="https://images.unsplash.com/photo-1536766768598-e09213fdcf22?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8YmVhdXRpZnVsJTIwbGFkeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
-              <span className="px-1 text-primary">Hi {user.firstName? user.firstName:",Welcome"}</span>
-              <ExpandMoreRoundedIcon color="primary" />
+              <Button
+                aria-owns={anchorEl ? "simple-menu" : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+                onMouseOver={handleClick}
+              >
+                              <CircleUserAvatar imgUrl="" />
+
+                <span className="capitalize font-body font-normal">
+                  <span className="px-1 text-primary">
+                    Hi {user.firstName ? user.firstName : ",Welcome"}
+                  </span>
+                  <ExpandMoreRoundedIcon color="primary" />
+                </span>
+              </Button>
             </button>
+            <Menu
+          id="menu-appbar"
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleClose}
+          MenuListProps={{ onMouseLeave: handleClose }}
+          PaperProps={{
+            style: {
+              left: "50%",
+              transform: "translateX(10%) translateY(75%)",
+            },
+          }}
+        >
+          <div>
+            <MenuItem
+              onClick={handleClose}
+              className={`${classes.menuItem} font-body font-normal flex`}
+            >
+              <span className="justify-self-start px-2">
+                <FiLogOut />
+              </span>
+              <span className="font-body font-normal flex-1 pr-8">
+                Log Out
+              </span>
+              
+            </MenuItem>
+          </div>
+        </Menu>
             {showFilter ? (
               <button className="flex items-center font-body">
                 <Filter /> <span className="pl-2 text-primary">Filter</span>
@@ -65,6 +134,4 @@ const mapStateToProps = (state) => {
     ...state,
   };
 };
-export default connect(mapStateToProps, {
-  
-})(HeaderRowOne);
+export default connect(mapStateToProps, {})(HeaderRowOne);
