4ded94d9a38fd68de5e6e4468c9f8b6654cd0b58
Mmojekwu Cyril <lovelyjhames@gmail.com>
2021-03-23 20:17:15 -0700

    update

--- src/Pages/ChooseYear.jsx
+++ src/Pages/ChooseYear.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import {
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      width: "80%",
+    },
+  },
+  numberSelect: {
+    width: "20ch",
+    paddingRight: "0.8rem !important",
+  },
+  menuPaper: {
+    maxHeight: 200,
+  },
+}));
 
 export default function ChooseYear() {
+  const classes = useStyles();
+
+  const [itemNumber, setitemNumber] = useState("");
+
+  const handleChange = (event) => {
+    setitemNumber(event.target.value);
+  };
+
+  const range = (start, end) => {
+    return Array(end - start + 1)
+      .fill()
+      .map((_, idx) => start + idx);
+  };
+
+  const rangeValue = range(2000, 2020);
+
   return (
     <div>
       {!localStorage.token ? <Redirect to="/login" /> : null}
@@ -12,8 +50,29 @@ export default function ChooseYear() {
         <div className="font-body flex flex-col gap-28 text-center mt-40">
           <h3 className="text-3xl font-body">BIOLOGY (SSCE) WAEC Questions</h3>
           <div className="flex items-center justify-center">
-            <span>... </span>
-            <span className="md:text-2xl pl-3">button</span>
+            <div className="text-center">
+              <FormControl variant="outlined">
+                <InputLabel id="demo-simple-select-outlined-label">
+                  Choose Year
+                </InputLabel>
+                <Select
+                  label="Choose Year"
+                  labelId="demo-simple-select-placeholder-label-label"
+                  id="demo-simple-select-placeholder-label"
+                  value={itemNumber}
+                  onChange={handleChange}
+                  displayEmpty
+                  className={classes.numberSelect}
+                  MenuProps={{ classes: { paper: classes.menuPaper } }}
+                >
+                  {rangeValue.map((e, i) => (
+                    <MenuItem value={i} key={i}>
+                      {e}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </div>
           </div>
           <div>
             <Link to="/pq/biology-choose-type">
