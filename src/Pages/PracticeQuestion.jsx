c15e86c39d43895ca70b06b45702002f9360479c
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-03-27 01:26:43 +0100

    Feat:implement desktop 59

--- src/Pages/PracticeQuestion.jsx
+++ src/Pages/PracticeQuestion.jsx
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
+
+
+
+
+
+
 import Pagination from "../components/AnswerContent/Pagination";
 import { DefaultAnswerBtn } from "../components/Button/AnswerButton";
 import FormControl from "@material-ui/core/FormControl";
 import PracticeHeader from "../components/Header/PracticeHeader";
 import NumberBadge from "../components/Badge/NumberBadge";
 import {questionArray} from "../DB/dummyQuestion"
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 
 export default function PracticeQuestion() {
@@ -32,14 +58,54 @@ export default function PracticeQuestion() {
     
   }
 
-  const submitPracticeQuestion=()=>{
-    console.log("answer submitted")
+  
 
-  }
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
       <PracticeHeader />
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Finish Past Question?</h2>
+            <p id="transition-modal-description">Are you sure you want to end this past question</p>
+            <div className="p-10">
+            <button onClick={()=>console.log("ok")} className="text-white bg-primary px-12 font-body shadow-primary px-5 py-2 rounded-md focus:outline-none text-sm lg:text-md font-medium">
+            Yes, Submit
+          </button>
+          </div>
+
+          <div className="p-10">
+            <button onClick={()=>handleClose("ok")}   className="text-primary  px-12 font-body shadow-primary px-5 py-2 rounded-md focus:outline-none text-sm lg:text-md font-medium">
+            No, Cancel
+          </button>
+          </div>
+          </div>
+        </Fade>
+      </Modal>
 
       <div className="max-w-3xl mx-auto">
         <div className="flex justify-between items-center pt-6 pb-6">
@@ -106,7 +172,7 @@ export default function PracticeQuestion() {
         </div>
         <div>
         {questionNumber===20? (
-          <button onClick={()=>submitPracticeQuestion()} className="text-white bg-primary px-12 font-body shadow-primary px-5 py-2 rounded-md focus:outline-none text-sm lg:text-md font-medium">
+          <button onClick={()=>handleOpen()} className="text-white bg-primary px-12 font-body shadow-primary px-5 py-2 rounded-md focus:outline-none text-sm lg:text-md font-medium">
           FINISH 
         </button>
 
