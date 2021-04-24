c88410b42eef5a54b8d120bde1b60e3e24d5c407
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-04-24 22:29:46 +0100

    fix

--- src/Pages/ChooseYear.jsx
+++ src/Pages/ChooseYear.jsx
@@ -3,6 +3,8 @@ import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import 'katex/dist/katex.min.css';
+import Latex from 'react-latex-next'
 import {
   FormControl,
   InputLabel,
@@ -12,6 +14,8 @@ import {
 } from "@material-ui/core";
 import MobileHeader from "../components/Header/MobileHeader";
 import { selectPastQuestionYear, } from "../redux/actions/practiceQuestion";
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -32,6 +36,7 @@ function ChooseYear(props) {
   //const {subject}=props.practiceQuestionReducer
   const classes = useStyles();
 
+
   const [itemNumber, setitemNumber] = useState();
 
   const handleChange = async(event) => {
@@ -48,6 +53,7 @@ function ChooseYear(props) {
   };
 
   const rangeValue = range(2000, 2020);
+  let latexString = "\[5_{2}-\frac{3}{4}\binom{4}{7}\]";
 
   return (
     <div>
@@ -59,7 +65,7 @@ function ChooseYear(props) {
       <div className="flex justify-center pb-40">
         <div className="font-body flex flex-col gap-28 text-center mt-12 sm:mt-40">
           <h3 className="px-3 text-md sm:text-xl md:text-2xl lg:text-3xl font-body">
-            BIOLOGY (SSCE) WAEC Questions
+            BIOLOGY (SSCE) WAEC Questions <Latex>$5_{2}-\frac{3}{4}\binom{4}{7}$ </Latex>
           </h3>
           <div className="flex items-center justify-center">
             <div className="text-center">
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,13 +3,14 @@ import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import LoginHeader from "../components/Auth/LoginHeader";
 import LoginBody from "../components/Auth/LoginBody";
-
 import { login, saveLoginUserDataToState,clearLoginRelatedErrors } from "../redux/actions/login";
 
 function Login(props) {
   clearLoginRelatedErrors()
+  
   return (
     <div className="bg-f8 pb-8 2xl:h-screen">
+     
       {localStorage.token ? <Redirect to="/pq/biology-choose-year" /> : null}
         <LoginHeader />
         <LoginBody loginRequest={props.login} error={props.loginReducer.error.message} />
