29ad72bda167a7e5679c10d03695829b8868296f
Okorie Chinedu <okoriechinedusunday@gmail.com>
2021-05-02 08:41:47 +0100

    fix: change base url

--- src/redux/actions/config.js
+++ src/redux/actions/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://awesumedge-api.herokuapp.com/api/v1/';
+export const BASE_URL = 'https://api.awesumedge.com/api/v1/';
 export const requestHeaders = {
     headers: {
         'content-type': 'application/json',
