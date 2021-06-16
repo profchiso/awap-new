da20a06c4528d195d565dae11dfac01535d499b1
Zonnaslyric <lovelyjhames@gmail.com>
2021-06-16 00:02:35 -0700

    timed ui update

--- src/components/PastQuestion/timedPqIntro.jsx
+++ src/components/PastQuestion/timedPqIntro.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { ReactComponent as QuestionMark } from "../../assets/svgs/QuestionMark.svg";
+import {ReactComponent as TimeLogo} from "../../assets/svgs/TimeLogo.svg";
 
 function TimedPqIntro(props) {
     const {
@@ -13,26 +14,27 @@ function TimedPqIntro(props) {
 
     return (
         <div className="flex justify-center mb-24 pb-40 sm:pb-0">
-            <div className="font-body flex flex-col gap-28 text-center mt-24 lg:mt-40">
+            <div className="font-body flex flex-col gap-16 text-center mt-24 lg:mt-24">
                 <h3 className="text-md sm:text-xl md:text-2xl lg:text-3xl font-body">
                     Timed {year} {subject} Past Questions
                 </h3>
                 <div className="flex justify-between">
-                    <div className="flex"><QuestionMark /><span className="md:text-2xl pl-3"> 20 Questions</span></div>
+                    <div className="flex"><QuestionMark /><span className="md:text-lg pl-3"> 20 Questions</span></div>
 
-                    <span className="md:text-2xl pl-3">60 minutes</span>
+                   <div  className="flex"><TimeLogo/> <span className="md:text-lg pl-3">60 minutes</span></div> 
                 </div>
                 <div className="text-left">
-                    <h3 className="text-md sm:text-xl md:text-2xl lg:text-2xl font-body">
+                    <h3 className="text-md sm:text-xl md:text-xl lg:text-2xl font-body">
                         Instructions
                     </h3>
-                </div>
-                <div className="text-left">
+                    <div className="text-left text-base pt-4 text-primary">
                     <p>1. &nbsp;&nbsp;Do not copy</p>
                     <p>2. &nbsp;&nbsp;Do not cheat</p>
                     <p>3. &nbsp;&nbsp;Do not let someone else do the work for you</p>
                 </div>
 
+                </div>
+              
                 {/* <div className="flex items-center justify-center">
 
 
