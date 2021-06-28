8e559599cffe88def95fcf125de99772f843eea9
Zonnaslyric <lovelyjhames@gmail.com>
2021-06-27 23:38:46 -0700

    mobile ui

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -149,7 +149,7 @@ function Statistics(props) {
           </div>
 
           <div>
-            <div className="flex -mt-16 -mb-10  sm:mt-8 sm:mb-0">
+            <div className="flex -mt-16 -mb-10  sm:mt-8 sm:mb-0 -z-10 relative">
               <DonutChart name="Donut" />
               <div className="flex"></div>
             </div>
