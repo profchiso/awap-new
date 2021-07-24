f223735b418e1a9436b52ae7f0b941db3f810966
Zonnaslyric <lovelyjhames@gmail.com>
2021-07-24 05:58:29 -0700

    commented search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { CgSearch } from "react-icons/cg";
+// import React from "react";
+// import { CgSearch } from "react-icons/cg";
 
 export default function Search() {
   return (
     <div className="flex flex-1 justify-center ml-4 mr-5 md:mx-10">
-      <form className="flex justify-self-center items-center flex-1  max-w-sm sm:max-w-lg">
+      {/* <form className="flex justify-self-center items-center flex-1  max-w-sm sm:max-w-lg">
         <CgSearch className="absolute text-xl text-primary ml-3" />
         <input
           type="text"
           className="py-2.5 px-4 pl-12 focus:outline-none w-full border text-black border-primary-400 rounded-full text-sm"
           placeholder="Search anything"
         />
-      </form>
+      </form> */}
     </div>
   );
 }
